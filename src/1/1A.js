58fc139d5581aa2437aa7c920cc1c52392c05aac
Albert Hambardzumyan <hambardzumyan.albert@gmail.com>
2020-04-07 21:55:23 +0400

    Problem 1A - Theatre Square.

---
+++ src/1/1A.js
@@ -0,0 +1,20 @@
+/**
+ * @author Albert Hambardzumyan
+ * @description Theatre Square.
+ *  Reference http://codeforces.com/problemset/problem/1/A
+ */
+(function () {
+  const input = readline().split(' ')
+
+  const m = input[0]
+  const n = input[1]
+  const a = input[2]
+
+  const height = m % a === 0 ? Math.floor(m / a) : Math.floor(m / a) + 1
+  const width = n % a === 0 ? Math.floor(n / a) : Math.floor(n / a) + 1
+
+  const result = height * width
+
+  print(result)
+})()
+
